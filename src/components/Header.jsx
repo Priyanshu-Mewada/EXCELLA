8cb3e5f129d361ea7d12a7029855b7db64d5048b
Priyanshu <131534045+Priyanshu-Mewada@users.noreply.github.com>
2025-10-29 00:12:12 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,3 @@
-// import react from 'react'
-
-// const Header = () => {
-//     return (
-//         <div className=' items-center justify-center bg-gray-500 p-2 shadow-lg m-2 rounded-lg'>
-//         <div className='flex flex-col items-center justify-center p-2'> 
-//                {/* <img src="./assets/ICON." alt="" /> */}
-               
-//                <h1 
-//             className=" text-3xl font-extrabold text-white  justify-center text-center ">
-//                 EXCELLA
-//             </h1>
-//             <p className='text-white'>AI Excel automation tool</p>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Header;
-
 import React from "react";
 
 const Header = () => {
